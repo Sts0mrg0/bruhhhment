f6d7907917a34bbd760e8676d761b590132e477a
Titus Ng <titusngiscoding@gmail.com>
2019-06-13 23:31:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const http = require("http");
 const net = require("net");
-const url = require("url");
 const httpProxy = require("http-proxy");
 const serverPort = 2004;
 
 var proxyServer = httpProxy.createProxyServer();
 var server = http.createServer(function(req, res){
   console.log(req.method + " " + req.url);
-  proxyServer.web(req, res, {target: {host: req.headers.host, port: req.port}});
+  proxyServer.web(req, res, {target: req.url});
 }).listen(serverPort);
 
 server.on("connect",function(req, socket, head){
   console.log(req.method + " " + req.url);
-  var proxySocket = net.createConnection(req.headers.host.split(":")[1], req.headers.host.split(":")[0], function(){
+  var proxySocket = net.createConnection(req.url.split(":")[1], req.url.split(":")[0], function(){
     proxySocket.write(head);
     socket.write("HTTP/1.1 200 Connection Established\r\n\r\n");
   });
